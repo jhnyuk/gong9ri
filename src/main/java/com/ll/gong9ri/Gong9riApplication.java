fdcb6559e7de1779f6b3e3407bad58aea3bf6daa
Youngwoo Choi <yw823x@daum.net>
2023-06-19 17:31:57 +0900

    Initial Commit

---
+++ src/main/java/com/ll/gong9ri/Gong9riApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gong9ri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gong9riApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gong9riApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/gong9ri/Gong9riApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ll.gong9ri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gong9riApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
