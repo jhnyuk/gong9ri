428ec7e19b00092090f56fd1bbc0a93883397256
Youngwoo Choi <yw823x@daum.net>
2023-07-11 21:51:40 +0900

    Chore: merge fix

--- src/main/java/com/ll/gong9ri/boundedContext/product/controller/ManageProductController.java
+++ src/main/java/com/ll/gong9ri/boundedContext/product/controller/ManageProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
