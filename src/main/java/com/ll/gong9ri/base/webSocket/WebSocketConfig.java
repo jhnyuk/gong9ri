124a6d15b6c8b1a3f113ba47f43c6f1a32f0ae44
kkambbak <yunsung9841@gmail.com>
2023-07-13 03:05:17 +0900

    HotFix: Enter chatRoom

--- src/main/java/com/ll/gong9ri/base/webSocket/WebSocketConfig.java
+++ src/main/java/com/ll/gong9ri/base/webSocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/chats").withSockJS();
+		registry.addEndpoint("/chats").setAllowedOriginPatterns("*").withSockJS();
 	}
 
 	@Override
